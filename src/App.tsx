93b461abb98db9b46e400624ec868b5c592bae14
prathamesh.tugaonkar <prathamesh.tugaonkar@jukshio.com>
2023-06-23 21:17:21 +0530

    client added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import './App.css'
+import 'dotenv/config';
 
-function App() {
+
+const socket = io('http://localhost:3000/api');
+
+const App = () => {
+  const [image, setImage] = useState('');
+  const [selectedRoom, setSelectedRoom] = useState('general');
+
+  useEffect(() => {
+    
+    socket.emit('joinRoom', selectedRoom);
+
+    socket.on('image', (receivedImage) => {
+      setImage(receivedImage);
+    });
+
+    return () => {
+      socket.emit('leaveRoom', selectedRoom);
+      socket.off('image');
+    };
+  }, [selectedRoom]);
+
+  const handleFileChange = (event:any) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64Image = reader.result;
+
+      // Send the base64 encoded image to the server
+      socket.emit('uploadImage', { room: selectedRoom, image: base64Image }, (response:any) => {
+        console.log(response); 
+      });
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleRoomChange = (event:any) => {
+    setSelectedRoom(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className='center'>
+      <h2>Image Uploader</h2>
+    </div><div className="container">
+        <div className="grid">
+          <div className="grid-item">
+            <input type="file" onChange={handleFileChange} />
+            <select value={selectedRoom} onChange={handleRoomChange}>
+              <option value="general">General</option>
+              <option value="faceimage">Face Image</option>
+            </select>
+          </div>
+          <div className="grid-item">
+            {image && <img src={image} alt="Received Image" />}
+          </div>
+        </div>
+      </div></>
+   
   );
-}
+};
 
 export default App;
