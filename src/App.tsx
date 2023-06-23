c734c39ec5939f44d8fa547680d0d259f035d0d1
Prathamesh <tprathamesh21@gmail.com>
2023-06-23 21:43:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import './App.css'
 
 
-const socket = io('https://react-node-socket-image-jbl1.vercel.app/api');
+const socket = io('https://react-node-socket-image.vercel.app/api');
 
 const App = () => {
   const [image, setImage] = useState('');
